e31e0419981d9add7d05c7232e678962f903e098
owl-app <owl@pawelkeska.eu>
2023-09-06 09:51:31 +0000

    Psalm fixes

--- Repository/EquipmentAttributeValueRepositoryInterface.php
+++ Repository/EquipmentAttributeValueRepositoryInterface.php
@@ -7,6 +7,11 @@ namespace Owl\Component\Equipment\Repository;
 use Owl\Component\Equipment\Model\EquipmentAttributeValueInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
+/**
+ * @template T of EquipmentAttributeValueInterface
+ *
+ * @extends RepositoryInterface<T>
+ */
 interface EquipmentAttributeValueRepositoryInterface extends RepositoryInterface
 {
     /**
