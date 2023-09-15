9c24463c62153eb75873077a1d4e3e42b8b2ee51
owl-app <owl@pawelkeska.eu>
2023-09-15 19:26:00 +0000

    Fix psalm

--- Model/EquipmentAttribute.php
+++ Model/EquipmentAttribute.php
@@ -8,19 +8,4 @@ use Owl\Component\Attribute\Model\Attribute as BaseAttribute;
 
 class EquipmentAttribute extends BaseAttribute implements EquipmentAttributeInterface
 {
-    /** @var string */
-    protected $name;
-
-    /**
-     * @return string
-     */
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): void
-    {
-        $this->name = $name;
-    }
 }
